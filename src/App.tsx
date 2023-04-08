9130b7a07ec01cdb74f82beebdcc631a74c19434
enjirtmnv <shkensinio@gmail.com>
2023-04-08 12:07:13 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+    const name: string = "opportunities";
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>{name}</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
